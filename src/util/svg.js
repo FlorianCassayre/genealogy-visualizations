4f77549cdb6b787ca29f73c82000e826fae09bf7
Florian Cassayre <florian.cassayre@gmail.com>
2021-01-21 15:37:58 +0100

    Fix text fitting

--- src/util/svg.js
+++ src/util/svg.js
@@ -4,21 +4,20 @@ function setFontSize(text, size) {
     setAttribute(text, 'font-size', size);
 }
 
-// Not yet working
 export function adaptTextSize(text, maxFontSize, fitSize) {
     if(text.getComputedTextLength() <= fitSize) {
         return text;
     }
 
-    let lower = 1, upper = maxFontSize;
-    const maxIterations = 10;
+    let lower = 0, upper = maxFontSize;
+    const maxIterations = 6;
     for(let i = 0; i < maxIterations; i++) {
         const mid = (lower + upper) / 2;
         setFontSize(text, mid);
         if(text.getComputedTextLength() <= fitSize) {
-            upper = mid;
-        } else {
             lower = mid;
+        } else {
+            upper = mid;
         }
     }
     setFontSize(text, lower);
